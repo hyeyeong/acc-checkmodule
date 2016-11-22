f96ab0e2589468e150849728cf3e9360fea67a33
hyeyeong <jag9123@gmail.com>
2016-11-22 23:57:13 +0900

    1122 implemented: message bundle

--- src/kr/ac/sm/epubacccheck/smil/SMILAccessibilityHandler.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.sm.epubacccheck.smil;
-
-import org.idpf.epubcheck.util.css.CssContentHandler;
-import org.idpf.epubcheck.util.css.CssErrorHandler;
-
-public class SMILAccessibilityHandler implements DefaultHandler
-{
-
-}
